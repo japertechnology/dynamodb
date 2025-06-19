a7c3980f19088ddaa8af0c43ce173aa598f64f6d
JAPER <eric.mourant@japer.technology>
2025-06-19 13:09:43 +1000

    test: use default credentials for local DynamoDB

--- test/test-helper.js
+++ test/test-helper.js
@@ -27,7 +27,13 @@ exports.mockDynamoDB = function () {
 };
 
 exports.realDynamoDB = function () {
-  var opts = { endpoint : 'http://localhost:8000', region: 'us-west-2', apiVersion: '2012-08-10' };
+  var opts = {
+    endpoint : 'http://localhost:8000',
+    region: 'us-west-2',
+    apiVersion: '2012-08-10',
+    accessKeyId: 'AKID',
+    secretAccessKey: 'SECRET'
+  };
   return new AWS.DynamoDB(opts);
 };
 
